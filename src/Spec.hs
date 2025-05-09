b3c54c615205da152ece01ab1bf623fe28b8d556
sofiiarrroyoo <sarroyo@frba.utn.edu.ar>
2025-05-09 18:53:08 -0300

    Update Spec.hs

--- src/Spec.hs
+++ src/Spec.hs
@@ -6,6 +6,27 @@ import Control.Exception (evaluate)
 
 correrTests :: IO ()
 correrTests = hspec $ do
-    describe "TP 5" $ do
-        it "test de prueba" $ do
-            2 + 2 `shouldBe` 4
+    describe "Hamburguesas" $ do
+        it "precio final de hamburguesa sin ingredientes" $ do
+            precioFinal (Hamburguesa 50 []) `shouldBe` 50
+
+        it "precio final suma base e ingredientes" $ do
+            precioFinal (Hamburguesa 10 [Carne, Cheddar, Pan]) `shouldBe` 42
+
+        it "agregar ingrediente al final" $ do
+            ingredientes (agregarIngrediente BaconDeTofu (Hamburguesa 5 [Pan])) `shouldBe` [Pan, BaconDeTofu]
+
+        it "agrandar agrega carne si ya tiene carne" $ do
+            ingredientes (agrandar (Hamburguesa 5 [Pan, Carne])) `shouldBe` [Pan, Carne, Carne]
+
+        it "agrandar agrega pollo si no tiene carne pero sí pollo" $ do
+            ingredientes (agrandar (Hamburguesa 5 [Pollo, Pan])) `shouldBe` [Pollo, Pan, Pollo]
+
+        it "agrandar no cambia nada si no tiene carne ni pollo" $ do
+            ingredientes (agrandar (Hamburguesa 5 [Pan, Cheddar])) `shouldBe` [Pan, Cheddar]
+
+        it "descuento al 50%" $ do
+            precioBase (descuento 50 (Hamburguesa 40 [])) `shouldBe` 20
+
+        it "pdepBurger tiene precio final 110" $ do
+            precioFinal pdepBurger `shouldBe` 110
