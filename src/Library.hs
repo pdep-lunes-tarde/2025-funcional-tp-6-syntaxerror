d2163edba06aa7c3d5da78581a3e5a54c9ff9c76
joaquinsanta <joaqsantacruz@frba.utn.edu.ar>
2025-05-10 13:33:20 -0300

    Update Library.hs

--- src/Library.hs
+++ src/Library.hs
@@ -1,18 +1,24 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use elem" #-}
 module Library where
 import PdePreludat
 
+
 data Ingrediente =
-    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras | BaconDeTofu
+    Carne | Pan | PanIntegral | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras | BaconDeTofu | Papas | PatiVegano
     deriving (Eq, Show)
 
 precioIngrediente Carne = 20
 precioIngrediente Pan = 2
+precioIngrediente PanIntegral =3
 precioIngrediente Panceta = 10
 precioIngrediente Cheddar = 10
 precioIngrediente Pollo =  10
 precioIngrediente Curry = 5
 precioIngrediente QuesoDeAlmendras = 15
 precioIngrediente BaconDeTofu = 12
+precioIngrediente Papas = 10
+precioIngrediente PatiVegano = 10
 
 data Hamburguesa = Hamburguesa {
     precioBase :: Number,
@@ -26,7 +32,13 @@ precioFinal hamburguesa = precioBase hamburguesa + sum (map precioIngrediente (i
 
 agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
 agregarIngrediente ingrediente hamburguesa = hamburguesa { ingredientes = ingredientes hamburguesa ++ [ingrediente] }
- 
+
+
+sacarIngrediente :: Hamburguesa -> Ingrediente -> Hamburguesa
+sacarIngrediente (Hamburguesa p ingrs) ing =
+    Hamburguesa p (filter (/= ing) ingrs)
+
+
 tiene :: Ingrediente -> Hamburguesa -> Bool
 tiene ingrediente = any (== ingrediente) . ingredientes
 
@@ -34,6 +46,7 @@ agrandar :: Hamburguesa -> Hamburguesa
 agrandar hamburguesa
   | tiene Carne hamburguesa = agregarIngrediente Carne hamburguesa
   | tiene Pollo hamburguesa = agregarIngrediente Pollo hamburguesa
+  | tiene PatiVegano hamburguesa = agregarIngrediente PatiVegano hamburguesa
   | otherwise     = hamburguesa
 
 descuento :: Number -> Hamburguesa -> Hamburguesa
@@ -50,3 +63,36 @@ pdepBurger =
     agregarIngrediente Panceta $
     agrandar $
     agrandar cuartoDeLibra
+
+-- PARTE 2
+
+dobleCuarto :: Hamburguesa
+dobleCuarto = Hamburguesa (precioBase cuartoDeLibra) (ingredientes cuartoDeLibra ++[Carne, Cheddar])
+
+bigPdep :: Hamburguesa
+bigPdep= Hamburguesa (precioBase dobleCuarto) (ingredientes dobleCuarto ++[Curry])
+
+delDia :: Hamburguesa -> Hamburguesa
+delDia hamburguesa = Hamburguesa (precioBase hamburguesa*0.7) (ingredientes hamburguesa ++[Papas])
+
+-- PARTE 3
+convertirVeggie :: Ingrediente -> Ingrediente
+convertirVeggie Carne = PatiVegano
+convertirVeggie Cheddar = QuesoDeAlmendras
+convertirVeggie Panceta = BaconDeTofu
+convertirVeggie otro = otro
+
+cambiarPan ::Ingrediente -> Ingrediente
+cambiarPan Pan = PanIntegral
+
+hacerVeggie :: Hamburguesa -> Hamburguesa
+hacerVeggie (Hamburguesa precio ingredientes) =
+    Hamburguesa precio (map convertirVeggie ingredientes)
+
+cambiarPanDePati :: Hamburguesa -> Hamburguesa
+cambiarPanDePati (Hamburguesa precio ingredientes)=
+  Hamburguesa precio (map cambiarPan ingredientes)
+
+dobleCuartoVegano :: Hamburguesa
+dobleCuartoVegano = cambiarPanDePati (hacerVeggie dobleCuarto)
+
